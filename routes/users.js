37b8a4e47e5613b4e087bddcb8b402dbbebb6018
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-09-14 16:51:24 +0200

    refactoring code to have user routers

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,53 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const User = require('../models/users');
+
+// creating Users endpoint
+router.post('/users', async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+// Updating Users endpoint
+router.patch('/users/:id', async (req, res) => {
+  try {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'password', 'age', 'email'];
+    const isValidUpdates = updates.every(
+        (update) => allowedUpdates.includes(update));
+    if (!isValidUpdates) {
+      return res.status(404).send('not valid updates');
+    }
+
+    const _id = req.params.id;
+    const user = await User.findByIdAndUpdate(
+        _id, req.body, {new: true, runValidators: true});
+    if (!user) {
+      return res.status(404).send();
+    }
+    return res.status(200).send(user);
+  } catch (e) {
+    return res.status(500).send(e);
+  }
+});
+
+// Deleting Users Account endpoint
+router.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    return res.status(200).send();
+  } catch (e) {
+    return res.status(500).send();
+  };
 });
 
 module.exports = router;
