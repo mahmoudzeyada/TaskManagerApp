1f2989b482314c7fbf16e81921f6a2a6c4635933
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-09-14 16:56:47 +0200

    refactoring tasks endpoint to be in seprate file

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
+
 const usersRouter = require('./routes/users');
+const tasksRouter = require('./routes/tasks');
 
 require('./models/dbConnections');
 const User = require('./models/users');
@@ -24,6 +25,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(usersRouter);
+app.use(tasksRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
