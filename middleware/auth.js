c4486e58937babc01ac3c17db188478f5e01d0d6
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-09-16 17:06:44 +0200

    building authentication middleware

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/users');
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const decode = jwt.verify(token, 'thisisasecretKey');
+    const user = await User.findOne({'_id': decode._id, 'tokens.token': token});
+    if (!user) {
+      throw new Error();
+    }
+    req.user = user;
+    next();
+  } catch (e) {
+    console.log(e);
+    res.status(401).send({error: 'authentication cardinalities are wrong'});
+  }
+};
+
+module.exports = auth;
