1f207dfb38b92897b21b65c4ed2e6609420614f6
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-09-24 00:18:46 +0200

    replacing secrets with env variables

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ const User = require('../models/users');
 const auth = async (req, res, next) => {
   try {
     const token = req.header('Authorization').replace('Bearer ', '');
-    const decode = jwt.verify(token, 'thisisasecretKey');
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findOne({'_id': decode._id, 'tokens.token': token});
     if (!user) {
       throw new Error();
