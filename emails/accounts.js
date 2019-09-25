15d84f996203bf7a03e3ddedb67e12a6209b49a5
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-09-25 21:02:47 +0200

    changing permission on files

--- emails/accounts.js
+++ emails/accounts.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
@@ -20,7 +21,22 @@ const sendCancellationEmail = (name, email) => {
   });
 };
 
+const sendResetPasswordEmail = (token, {email, name}) => {
+  sgMail.send({
+    to: email,
+    from: 'mahmoudzeyada440@gmail.com',
+    subject: 'Reset Password Request!',
+    text: `Hi ${name} receiving this e-mail because you requested a password reset for your user account at PythonAnywhere.
+          Please go to the following page and choose a new password:
+          http://${process.env.url}/forget_password/confirm/${token}/
+          Your username, in case you've forgotten: ${name}
+          Thanks for using our site!
+          PS (this url we be valid for only 15 minutes if you did not send this request please just ignore it)`,
+  });
+};
+
 module.exports = {
   sendWelcomeEmail,
   sendCancellationEmail,
+  sendResetPasswordEmail,
 };
